4719a2f623c839ca795fbe52ee26ee1b8e101fd6
tanvirch0750 <tanvir0750@yahoo.com>
2022-04-17 01:16:21 +0600

    implemented heading component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import AboutMeHome from "../../components/About-me-home/AboutMeHome";
 import Banner from "../../components/Banner/Banner";
+import Heading from "../../components/Heading/Heading";
 
 const Home = () => {
   return (
     <>
       <Banner />
       <AboutMeHome />
+      <Heading
+        heading="Admit it. You've considered avoiding your taxes altogether."
+        text="But you don't have to. I'm here to help make sure you get back every dime you're entitled to."
+      />
     </>
   );
 };
