d80870d55a7fe34e1efb507c55880147566dce90
tanvirch0750 <tanvir0750@yahoo.com>
2022-04-17 20:19:21 +0600

    styled book now form section

--- src/pages/Checkout/Checkout.jsx
+++ src/pages/Checkout/Checkout.jsx
@@ -5,6 +5,7 @@ import { BsFillCameraVideoFill } from "react-icons/bs";
 import { HiClock, HiCreditCard } from "react-icons/hi";
 import { useParams } from "react-router-dom";
 import auth from "../../Authentication/Firebase/Firebase.init";
+import PageHeadImg from "../../components/PageHeadingImg/PageHeadImg";
 import servicesDb from "../../utilities/servicesDb";
 import "./Checkout.css";
 
@@ -26,81 +27,89 @@ const Checkout = () => {
   const service = servicesDb.find((service) => service.id === +params.id);
 
   return (
-    <section className="checkout">
-      <div className="container">
-        <div className="checkout-container">
-          <div className="book-details">
-            <strong className="book-details-self-name">Larry Brandon</strong>
-            <h2>{service?.name}</h2>
-            <div className="book-details-info">
-              <div>
-                <HiClock className="icon" />
-                <strong>{service.time}</strong>
+    <>
+      <PageHeadImg text={`Book - ${service.name}`} />
+      <section className="checkout">
+        <div className="container">
+          <div className="checkout-container">
+            <div className="book-details">
+              <strong className="book-details-self-name">Larry Brandon</strong>
+              <h2>{service?.name}</h2>
+              <div className="book-details-info">
+                <div>
+                  <HiClock className="icon" />
+                  <strong>{service.time}</strong>
+                </div>
+                <div>
+                  <BsFillCameraVideoFill className="icon" />
+                  <strong>Conferencing details upon confirmation.</strong>
+                </div>
+                <div>
+                  <HiCreditCard className="icon" />
+                  <strong>{service.price}</strong>
+                </div>
               </div>
-              <div>
-                <BsFillCameraVideoFill className="icon" />
-                <strong>Conferencing details upon confirmation.</strong>
+              <div className="book-details-text">
+                <p>{service.text}</p>
               </div>
-              <div>
-                <HiCreditCard className="icon" />
-                <strong>{service.price}</strong>
-              </div>
+              <p className="book-thank-you">Thank you for work with me.</p>
             </div>
-            <div className="book-details-text">
-              <p>{service.text}</p>
-            </div>
-          </div>
-          <div className="book-form">
-            <form onSubmit={handleSubmit(onSubmit)}>
-              <h2>Fill this from for booking</h2>
-              <div className="book-form-input-control">
-                <input
-                  {...register("name", {
-                    required: "Please give your name",
-                    minLength: { value: 4, message: "Minimum length is four" },
-                  })}
-                  type="text"
-                  placeholder="Enter your name"
-                />
-                <p>{errors.name?.message}</p>
-              </div>
-              <div className="book-form-input-control">
-                <input
-                  {...register("email", {
-                    required: "Please provide a valid email address",
-                  })}
-                  type="email"
-                  placeholder="Enter your email"
-                />
-                <p>{errors.email?.message}</p>
-              </div>
+            <div className="book-form">
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <strong className="book-details-self-name">Good Choice</strong>
+                <h2>Fill this from for booking</h2>
+                <div className="book-form-input-control">
+                  <input
+                    {...register("name", {
+                      required: "Please give your name",
+                      minLength: {
+                        value: 4,
+                        message: "Minimum length is four",
+                      },
+                    })}
+                    type="text"
+                    placeholder="Enter your name"
+                  />
+                  <p>{errors.name?.message}</p>
+                </div>
+                <div className="book-form-input-control">
+                  <input
+                    {...register("email", {
+                      required: "Please provide a valid email address",
+                    })}
+                    type="email"
+                    placeholder="Enter your email"
+                  />
+                  <p>{errors.email?.message}</p>
+                </div>
 
-              <div className="book-form-input-control">
-                <input
-                  {...register("phoneNumber", {
-                    required: "Please provide your phone number",
-                  })}
-                  type="number"
-                  placeholder="Enter your phone number"
-                />
-                <p>{errors.phoneNumber?.message}</p>
-              </div>
-              <div className="book-form-input-control">
-                <input
-                  {...register("address", {
-                    required: "Please provide your address",
-                  })}
-                  type="text"
-                  placeholder="Enter your address"
-                />
-                <p>{errors.address?.message}</p>
-              </div>
-              <input type="submit" />
-            </form>
+                <div className="book-form-input-control">
+                  <input
+                    {...register("phoneNumber", {
+                      required: "Please provide your phone number",
+                    })}
+                    type="number"
+                    placeholder="Enter your phone number"
+                  />
+                  <p>{errors.phoneNumber?.message}</p>
+                </div>
+                <div className="book-form-input-control">
+                  <input
+                    {...register("address", {
+                      required: "Please provide your address",
+                    })}
+                    type="text"
+                    placeholder="Enter your address"
+                  />
+                  <p>{errors.address?.message}</p>
+                </div>
+                <input type="submit" className="btn" value="Book Now" />
+              </form>
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
 
