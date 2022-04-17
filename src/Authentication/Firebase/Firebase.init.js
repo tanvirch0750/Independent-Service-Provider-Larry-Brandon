a0532c0d9e84b01cc09aac561e5494e36d93640e
tanvirch0750 <tanvir0750@yahoo.com>
2022-04-18 03:13:00 +0600

    implemented firebase config

--- src/Authentication/Firebase/Firebase.init.js
+++ src/Authentication/Firebase/Firebase.init.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDyUTjCWoaCgcnsHjyZI-hDLpqYE6QC4G4",
-  authDomain: "larry-brandon.firebaseapp.com",
-  projectId: "larry-brandon",
-  storageBucket: "larry-brandon.appspot.com",
-  messagingSenderId: "517114593246",
-  appId: "1:517114593246:web:2d2f357095948f9d7ca74f",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
