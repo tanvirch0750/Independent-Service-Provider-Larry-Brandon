a049bf53264318ac3d9f5cb23f708d5a520d01db
tanvirch0750 <tanvir0750@yahoo.com>
2022-04-18 16:13:22 +0600

    Make home page responsive

--- src/components/About-me-home/AboutMeHome.jsx
+++ src/components/About-me-home/AboutMeHome.jsx
@@ -6,38 +6,40 @@ import "./AboutMeHome.css";
 const AboutMeHome = () => {
   const navigate = useNavigate();
   return (
-    <section className="about-me-home container">
-      <div className="about-me-home-img-box">
-        <img src={selfImg} alt="Larry Brabdon" />
-      </div>
-      <div className="about-me-home-content">
-        <div className="details-content">
-          <h3>Squash tax-time confusion</h3>
-          <h2>
-            I specialize in helping you understand how these life events can
-            make an impact on your tax return—and how you can start planning now
-            to make the best of it.
-          </h2>
+    <div className="container">
+      <section className="about-me-home">
+        <div className="about-me-home-img-box">
+          <img src={selfImg} alt="Larry Brabdon" />
         </div>
-        <div className="list-content">
-          <ul className="list-content-items">
-            <li>Marriage and Divorce</li>
-            <li>Purchasing a Home</li>
-            <li>Starting or Selling a Business</li>
-            <li>Retirement</li>
-            <li>Having a Baby</li>
-            <li>Military Transitions</li>
-            <li>And More</li>
-          </ul>
+        <div className="about-me-home-content">
+          <div className="details-content">
+            <h3>Squash tax-time confusion</h3>
+            <h2>
+              I specialize in helping you understand how these life events can
+              make an impact on your tax return—and how you can start planning
+              now to make the best of it.
+            </h2>
+          </div>
+          <div className="list-content">
+            <ul className="list-content-items">
+              <li>Marriage and Divorce</li>
+              <li>Purchasing a Home</li>
+              <li>Starting or Selling a Business</li>
+              <li>Retirement</li>
+              <li>Having a Baby</li>
+              <li>Military Transitions</li>
+              <li>And More</li>
+            </ul>
+          </div>
+          <button
+            onClick={() => navigate("/work-with-me")}
+            className="btn btn-primary"
+          >
+            My Services
+          </button>
         </div>
-        <button
-          onClick={() => navigate("/work-with-me")}
-          className="btn btn-primary"
-        >
-          My Services
-        </button>
-      </div>
-    </section>
+      </section>
+    </div>
   );
 };
 
